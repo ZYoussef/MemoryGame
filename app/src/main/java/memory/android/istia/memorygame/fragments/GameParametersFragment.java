d7ff2cc3d969e1ed5cc56dd3bb8818e7238aea9b
Tada <thomas.bernard72@gmail.com>
2018-12-29 17:36:33 +0100

    Début du jeu en lui même (juste des images fixe rn)

--- app/src/main/java/memory/android/istia/memorygame/fragments/GameParametersFragment.java
+++ app/src/main/java/memory/android/istia/memorygame/fragments/GameParametersFragment.java
@@ -1,17 +1,19 @@
 package memory.android.istia.memorygame.fragments;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import memory.android.istia.memorygame.R;
+import memory.android.istia.memorygame.utils.FragmentController;
 
 
-public class GameParametersFragment extends Fragment {
+public class GameParametersFragment extends Fragment implements View.OnClickListener {
+
+    private Button mButtonPlay;
 
     public GameParametersFragment() {
         // Required empty public constructor
@@ -26,7 +28,20 @@ public class GameParametersFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game_parameters, container, false);
+        View view = inflater.inflate(R.layout.fragment_game_parameters, container, false);
+        mButtonPlay = view.findViewById(R.id.buttonGameParameterPlay);
 
+
+        mButtonPlay.setOnClickListener(this);
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.buttonGameParameterPlay:
+                FragmentController.getInstance().openFragment(FragmentController.Fragments.GAME);
+                break;
+        }
     }
 }
